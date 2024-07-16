8a99b262a96c0e8c003f1d5eb40f9cbcb2920a6c
Hyrozdmr <hayriozdemir@hotmail.co.uk>
2024-07-16 11:14:37 +0100

    first commit

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return render_template('index.html')
+  return "Hello, world! Here's my favorite film quote: 'May the Force be with you.'"
+  
 
 
 if __name__ == "__main__":
